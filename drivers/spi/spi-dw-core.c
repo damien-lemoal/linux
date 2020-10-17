f11568974967b0af5e2d440a91d8079b28adfa6e
Damien Le Moal <damien.lemoal@wdc.com>
2020-10-17 10:47:18 +0900

    spi: dw: Improve dw_reader()
    On slow systems, relying on interrupts for signaling the presence of
    bytes to read in the master FIFO can be too slow and result in many FIFO
    overrun errors. This can be observed on the RISC-V Kendryte K210 SoC
    for large read IOs to an SD card. Improve dw_reader() to reduce the
    occurence of these overrun errors by adding an extra loop around the
    DW_SPI_DR read loop. This allows catching data that was added to the
    FIFO by the device while the previous loop was executing, thus
    improving receive rates.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- drivers/spi/spi-dw-core.c
+++ drivers/spi/spi-dw-core.c
@@ -159,24 +159,31 @@ static void dw_writer(struct dw_spi *dws)
 
 static bool dw_reader(struct dw_spi *dws)
 {
-	bool done;
+	bool done = false;
 	u32 max;
 	u16 rxw;
 
 	spin_lock(&dws->buf_lock);
-	max = rx_max(dws);
-	while (max--) {
-		rxw = dw_read_io_reg(dws, DW_SPI_DR);
-		/* Care rx only if the transfer's original "rx" is not null. */
-		if (dws->rx_end - dws->len) {
-			if (dws->n_bytes == 1)
-				*(u8 *)(dws->rx) = rxw;
-			else
-				*(u16 *)(dws->rx) = rxw;
+	while (!done) {
+		max = rx_max(dws);
+		if (!max)
+			break;
+		while (max--) {
+			rxw = dw_read_io_reg(dws, DW_SPI_DR);
+			/*
+			 * Set rx only if the transfer's original "rx"
+			 * is not null.
+			 */
+			if (dws->rx_end - dws->len) {
+				if (dws->n_bytes == 1)
+					*(u8 *)(dws->rx) = rxw;
+				else
+					*(u16 *)(dws->rx) = rxw;
+			}
+			dws->rx += dws->n_bytes;
 		}
-		dws->rx += dws->n_bytes;
+		done = (dws->rx_end == dws->rx);
 	}
-	done = (dws->rx_end == dws->rx);
 	spin_unlock(&dws->buf_lock);
 
 	return done;

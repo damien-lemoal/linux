c07aa2bf8555c42b810825fc32ccd91cfebdecc4
Atish Patra <atish.patra@wdc.com>
2020-11-18 16:38:29 -0800

    riscv: Add numa support for riscv64 platform
    Use the generic numa implementation to add NUMA support for RISC-V.
    This is based on Greentime's patch[1] but modified to use generic NUMA
    implementation and few more fixes.
    
    [1] https://lkml.org/lkml/2020/1/10/233
    
    Co-developed-by: Greentime Hu <greentime.hu@sifive.com>
    Signed-off-by: Greentime Hu <greentime.hu@sifive.com>
    Signed-off-by: Atish Patra <atish.patra@wdc.com>
    Reviewed-by: Anup Patel <anup@brainfault.org>
    Reviewed-by: Palmer Dabbelt <palmerdabbelt@google.com>
    Signed-off-by: Palmer Dabbelt <palmerdabbelt@google.com>

--- arch/riscv/mm/init.c
+++ arch/riscv/mm/init.c
@@ -21,6 +21,7 @@
 #include <asm/soc.h>
 #include <asm/io.h>
 #include <asm/ptdump.h>
+#include <asm/numa.h>
 
 #include "../kernel/head.h"
 
@@ -211,7 +212,6 @@ void __init setup_bootmem(void)
 	early_init_fdt_scan_reserved_mem();
 	dma_contiguous_reserve(dma32_phys_limit);
 	memblock_allow_resize();
-	memblock_dump_all();
 }
 
 #ifdef CONFIG_MMU
@@ -666,8 +666,10 @@ void __init paging_init(void)
 
 void __init misc_mem_init(void)
 {
+	arch_numa_init();
 	sparse_init();
 	zone_sizes_init();
+	memblock_dump_all();
 }
 
 #ifdef CONFIG_SPARSEMEM_VMEMMAP

a7c266b0111158ddcae163c7fffd46f86f9a5ac8
Damien Le Moal <damien.lemoal@opensource.wdc.com>
2022-01-28 10:55:31 +0900

    scsi: pm8001: Fix local variable declaration in pm80xx_pci_mem_copy()
    Declare the local variable destination1 as a pointer to a __le32 value
    rather than a u32. This suppresses the sparse warning:
    
    warning: incorrect type in assignment (different base types)
        expected unsigned int [usertype]
        got restricted __le32 [usertype]
    
    Signed-off-by: Damien Le Moal <damien.lemoal@opensource.wdc.com>

--- drivers/scsi/pm8001/pm80xx_hwi.c
+++ drivers/scsi/pm8001/pm80xx_hwi.c
@@ -71,14 +71,13 @@ static void pm80xx_pci_mem_copy(struct pm8001_hba_info  *pm8001_ha, u32 soffset,
 				u32 dw_count, u32 bus_base_number)
 {
 	u32 index, value, offset;
-	u32 *destination1;
-	destination1 = (u32 *)destination;
+	__le32 *destination1 = (__le32 *)destination;
 
 	for (index = 0; index < dw_count; index += 4, destination1++) {
 		offset = (soffset + index);
 		if (offset < (64 * 1024)) {
 			value = pm8001_cr32(pm8001_ha, bus_base_number, offset);
-			*destination1 =  cpu_to_le32(value);
+			*destination1 = cpu_to_le32(value);
 		}
 	}
 	return;

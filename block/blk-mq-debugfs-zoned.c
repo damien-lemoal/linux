b139b852a3ff1ce6670487ffbff248b98c3ab42b
Damien Le Moal <dlemoal@kernel.org>
2024-01-08 17:04:43 +0900

    block: Remove zone write locking
    Zone write locking is now unused and replaced with zone write plugging.
    Remove all code that was implementing zone write locking, that is, the
    various helper functions controlling request zone write locking and
    the gendisk attached zone bitmaps.
    
    The "zone_wlock" mq-debugfs entry that was listing zones that are
    write-locked is replaced with the zone_plugged_wplugs entry which lists
    the number of zones that have a zone write plug throttling write
    operations.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- block/blk-mq-debugfs-zoned.c
+++ block/blk-mq-debugfs-zoned.c
@@ -3,19 +3,19 @@
  * Copyright (C) 2017 Western Digital Corporation or its affiliates.
  */
 
-#include <linux/blkdev.h>
+#include "blk.h"
 #include "blk-mq-debugfs.h"
 
-int queue_zone_wlock_show(void *data, struct seq_file *m)
+int queue_zone_plugged_wplugs_show(void *data, struct seq_file *m)
 {
 	struct request_queue *q = data;
 	unsigned int i;
 
-	if (!q->disk->seq_zones_wlock)
+	if (!q->disk->zone_wplugs)
 		return 0;
 
 	for (i = 0; i < q->disk->nr_zones; i++)
-		if (test_bit(i, q->disk->seq_zones_wlock))
+		if (blk_zone_wplug_plugged(q->disk, i))
 			seq_printf(m, "%u\n", i);
 
 	return 0;

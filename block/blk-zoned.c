7678f1cbef650007e811bba3b146954c0fdb893d
Damien Le Moal <dlemoal@kernel.org>
2024-01-17 09:41:22 +0900

    block: Simplify blk_revalidate_zone_cb()
    The nr_zones field of struct blk_revalidate_zone_args is used by
    blk_revalidate_zone_cb() only to allocate the array of zone write plugs
    if that array is not already allocated. But this allocation does not
    need to be in blk_revalidate_zone_cb() and can be done in
    blk_revalidate_disk_zones() when preparing the blk_revalidate_zone_args
    structure for the report zones operation.
    
    Move the zone write plug array allocation to blk_revalidate_disk_zones()
    and remove the nr_zones field from struct blk_revalidate_zone_args.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- block/blk-zoned.c
+++ block/blk-zoned.c
@@ -920,7 +920,6 @@ void disk_free_zone_resources(struct gendisk *disk)
 
 struct blk_revalidate_zone_args {
 	struct gendisk	*disk;
-	unsigned int	nr_zones;
 	struct blk_zone_wplug *zone_wplugs;
 	sector_t	sector;
 };
@@ -966,12 +965,6 @@ static int blk_revalidate_zone_cb(struct blk_zone *zone, unsigned int idx,
 		return -ENODEV;
 	}
 
-	if (!args->zone_wplugs) {
-		args->zone_wplugs = blk_zoned_alloc_write_plugs(args->nr_zones);
-		if (!args->zone_wplugs)
-			return -ENOMEM;
-	}
-
 	/* Check zone type */
 	switch (zone->type) {
 	case BLK_ZONE_TYPE_CONVENTIONAL:
@@ -1011,7 +1004,7 @@ int blk_revalidate_disk_zones(struct gendisk *disk)
 	sector_t zone_sectors = q->limits.chunk_sectors;
 	sector_t capacity = get_capacity(disk);
 	struct blk_revalidate_zone_args args = { };
-	unsigned int noio_flag;
+	unsigned int nr_zones, noio_flag;
 	int ret;
 
 	if (WARN_ON_ONCE(!blk_queue_is_zoned(q)))
@@ -1036,12 +1029,16 @@ int blk_revalidate_disk_zones(struct gendisk *disk)
 		return -ENODEV;
 	}
 
+	nr_zones = (capacity + zone_sectors - 1) >> ilog2(zone_sectors);
+
 	/*
 	 * Ensure that all memory allocations in this context are done as if
 	 * GFP_NOIO was specified.
 	 */
 	args.disk = disk;
-	args.nr_zones = (capacity + zone_sectors - 1) >> ilog2(zone_sectors);
+	args.zone_wplugs = blk_zoned_alloc_write_plugs(nr_zones);
+	if (!args.zone_wplugs)
+		return -ENOMEM;
 
 	noio_flag = memalloc_noio_save();
 	ret = disk->fops->report_zones(disk, 0, UINT_MAX,
@@ -1068,7 +1065,7 @@ int blk_revalidate_disk_zones(struct gendisk *disk)
 	 */
 	blk_mq_freeze_queue(q);
 	if (ret > 0) {
-		disk->nr_zones = args.nr_zones;
+		disk->nr_zones = nr_zones;
 		swap(disk->zone_wplugs, args.zone_wplugs);
 		ret = 0;
 	} else {
@@ -1077,7 +1074,7 @@ int blk_revalidate_disk_zones(struct gendisk *disk)
 	}
 	blk_mq_unfreeze_queue(q);
 
-	blk_zoned_free_write_plugs(disk, args.zone_wplugs, args.nr_zones);
+	blk_zoned_free_write_plugs(disk, args.zone_wplugs, nr_zones);
 
 	return ret;
 }

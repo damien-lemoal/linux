88063342228171575bb885138085a7482adb2962
Damien Le Moal <dlemoal@kernel.org>
2024-10-23 17:22:55 +0900

    nvme: Add PCI transport type
    Define the transport type NVMF_TRTYPE_PCI (253) for PCI endpoint
    targets. The helper functions nvmet_is_pci_ctrl() and nvmet_is_pci_req()
    are also introduced to help identify (respectively) a target controller
    using the PCI transport and command requests issued from a PCI target
    controller.
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- drivers/nvme/target/nvmet.h
+++ drivers/nvme/target/nvmet.h
@@ -640,6 +640,16 @@ static inline bool nvmet_is_disc_subsys(struct nvmet_subsys *subsys)
     return subsys->type != NVME_NQN_NVME;
 }
 
+static inline bool nvmet_is_pci_ctrl(struct nvmet_ctrl *ctrl)
+{
+	return ctrl->port->disc_addr.trtype == NVMF_TRTYPE_PCI;
+}
+
+static inline bool nvmet_is_pci_req(struct nvmet_req *req)
+{
+	return nvmet_is_pci_ctrl(req->sq->ctrl);
+}
+
 #ifdef CONFIG_NVME_TARGET_PASSTHRU
 void nvmet_passthru_subsys_free(struct nvmet_subsys *subsys);
 int nvmet_passthru_ctrl_enable(struct nvmet_subsys *subsys);
